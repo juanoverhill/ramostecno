68340a926ffe8460ee4b8cf963f85f46ee64656e
Juan Arias <juanariasmail@gmail.com>
2018-08-15 17:03:00 -0300

    cambios ultimos

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -1,4 +1,6 @@
+import { FirestoreService } from './../../services/f-base.service';
 import { Component, OnInit } from '@angular/core';
+import { CalendarComponentOptions, DayConfig } from 'ion4-calendar';
 
 @Component({
   selector: 'app-calendar',
@@ -7,12 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPage implements OnInit {
 
+  daysConfig: DayConfig[] = [];
+  nDias: any;
+
   date: string;
   type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+  options: CalendarComponentOptions = {
+    weekdays: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+    pickMode: 'single',
+    daysConfig: this.daysConfig
+  };
 
-  constructor() { }
+  constructor(private fBase: FirestoreService) {
+   }
 
   ngOnInit() {
+    this.getDiasAnulados();
+    this.anular_n_dias(this.nDias);
+  }
+
+  getDiasAnulados() {
+    
+  }
+
+  anular_n_dias(n) {
+    for (let i = 0; i <= n; i++) {
+      this.daysConfig.push({
+        date: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getUTCDate() + i),
+        subTitle: '--',
+        disable: true
+      });
+    }
   }
 
 }
