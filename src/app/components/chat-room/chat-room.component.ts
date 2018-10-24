cd32d1cbb23b1afcb0cdd13139ec1ca7b9caa900
Juan Luis Arias <juanluisarias@MacBook-Pro-de-Juan.local>
2018-10-23 22:02:37 -0300

    chats

--- src/app/components/chat-room/chat-room.component.ts
+++ src/app/components/chat-room/chat-room.component.ts
@@ -1,9 +1,10 @@
 import { ChatRoom } from './../../../Model/models';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FirestoreService } from '../../../services/f-base.service';
-import { NavParams, ModalController } from '@ionic/angular';
+import { NavParams, ModalController, Content, List } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-chat-room',
@@ -12,25 +13,60 @@ import { Observable } from 'rxjs';
 })
 export class ChatRoomComponent implements OnInit {
 
+  @ViewChild(Content) contentArea: Content;
+  @ViewChild(List, {read: ElementRef}) chatList: ElementRef;
+
   messages: Observable<ChatRoom[]>;
+  autenticado = false;
+  usuario;
+  private mutationObserver: MutationObserver;
 
   constructor(private fb: FirestoreService, public navParams: NavParams,
-    private router: Router, private modalCtrl: ModalController) { }
+    private router: Router, private modalCtrl: ModalController, private auth: AuthService) { }
 
   ngOnInit() {
-    this.messages = this.fb.colWithIds$('CHAT_ROOM');
-    window.scrollTo(0, document.body.scrollHeight);
+
+    this.mutationObserver = new MutationObserver((mutations) => {
+      this.contentArea.scrollToBottom();
+  });
+
+  this.mutationObserver.observe(this.chatList.nativeElement, {
+      childList: true
+  });
+
+    // Verifico previamente si esta logueado
+    this.auth.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.autenticado = true;
+        this.usuario = user.uid;
+        // Obtengo los mensajes
+        this.messages = this.fb.colWithIds$('CHAT_ROOM', ref => ref.orderBy('createdAt'));
+      } else {
+        this.login();
+      }
+    }, () => {
+      this.login();
+    }
+    );
   }
 
-  newMessagge(message) {
+  login() {
+    this.router.navigateByUrl('loguear');
+  }
+
+  newMessagge() {
+    const textoMessage = document.getElementById('mens') as HTMLIonTextareaElement;
+
     const newMess = new ChatRoom();
-    newMess.mensaje = message;
-    newMess.usuario_id = '';
+    newMess.mensaje = textoMessage.value;
+    newMess.usuario_id = this.usuario;
     newMess.time = new Date();
     newMess.empresa = 'RamosTecno';
     newMess.leido = false;
-    newMess.sender = true;
+    newMess.sender = false;
     this.fb.add('CHAT_ROOM', newMess);
+
+    textoMessage.value = '';
   }
 
 }
