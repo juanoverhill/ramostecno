05a725a6bf523f2a4d322649bcb10770b7d9eeca
Juan Luis Arias <juanluisarias@MacBook-Pro-de-Juan.local>
2018-10-24 21:14:19 -0300

    chatChange

--- src/Model/models.ts
+++ src/Model/models.ts
@@ -141,4 +141,5 @@ export class ChatRoom {
     usuario_id?: string;
     empresa?: string;
     sender?: boolean;
+    sender_id?: string;
 }
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,16 +2,17 @@ import { DocPipe } from './doc.pipe';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { DiaSemanaPipe } from './dia-semana.pipe';
 import { MenuComponent } from './components/menu/menu.component';
+import { FirestoreDatePipe } from './firestore-date.pipe';
 
 
 
 @NgModule({
     imports: [],
     declarations: [
-        DocPipe, DiaSemanaPipe
+        DocPipe, DiaSemanaPipe, FirestoreDatePipe
     ],
     exports: [
-        DocPipe, DiaSemanaPipe
+        DocPipe, DiaSemanaPipe, FirestoreDatePipe
     ],
     entryComponents: []
 })
